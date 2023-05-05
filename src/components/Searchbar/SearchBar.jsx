4959ef45972eaae2e34695f9b071ca71f7eecca9
amar kumar <amarkumar.sharma.124@gmail.com>
2023-05-05 09:21:34 +0530

    search ui created

--- src/components/Searchbar/SearchBar.jsx
+++ src/components/Searchbar/SearchBar.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import './searchBar.css'
-
+import { BiSearchAlt2 } from "react-icons/bi";
 const SearchBar = () => {
-  return (
-	<div className='searchbar'>
-		<h2>Find most populer movies and TV shows here. </h2>
-
-	</div>
-  )
+	return (
+		<div className='searchbar__container'>
+			<div className='searchbar__text'>
+				<h1>Find most populer movies and TV shows here. </h1>
+			</div>
+			<div className='search'>
+				<input type="text" placeholder='Search Shows Name Hear' />
+				<BiSearchAlt2 style={{ fontSize: 22, position: "relative", right: 30, top: 20, color: '#926000'}}/>
+			</div>
+		</div>
+	)
 }
 
 export default SearchBar
