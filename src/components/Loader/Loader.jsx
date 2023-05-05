a216408db8a800d8e89aa1ab43d68cbcbfdff476
amar kumar <amarkumar.sharma.124@gmail.com>
2023-05-05 10:02:44 +0530

    distorted css fixed

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import { Circles } from 'react-loader-spinner'
+import { ThreeDots } from 'react-loader-spinner'
 import './loader.css'
 
 const Loader = () => {
-  return (
-	<div className='loader'>
-		  <Circles
-			  height="50"
-			  width="50"
-			  color="#926000"
-			  ariaLabel="circles-loading"
-			  wrapperStyle={{}}
-			  wrapperClass=""
-			  visible={true}
-		  />
-	</div>
-  )
+	return (
+		<div className='loader'>
+			<ThreeDots
+				height="80"
+				width="80"
+				radius="9"
+				color="#926000"
+				ariaLabel="three-dots-loading"
+				wrapperStyle={{}}
+				wrapperClassName=""
+				visible={true}
+			/>
+		</div>
+	)
 }
 
 export default Loader
